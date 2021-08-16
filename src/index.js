56f6df21bd433a6c704891213eaa490703176167
tonyccm2 <tonyccm2@gmail.com>
2021-08-16 12:50:01 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const {app, BrowserWindow, Menu} = require('electron');
+
+app.on('ready', () => {
+    const win = new BrowserWindow({
+        width: 1200,
+        heigth: 900,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+        }
+    })
+    win.loadFile('git/src/views/index.html')
+    const mainMenu = Menu.buildFromTemplate(templateMenu);
+    Menu.setApplicationMenu(mainMenu);
+});
+
+const templateMenu = [
+    {
+        label: 'DevTools',
+        submenu: [
+            {
+                label: 'Dev tools',
+                click(item, focusedWindow){
+                    focusedWindow.toggleDevTools();
+                }
+            }
+        ]
+    }
+]
