ba9952358cec67a68e6de47d572c6721b36eaf2c
tonyccm2 <tonyccm2@gmail.com>
2021-08-16 21:08:12 -0500

    funcionalidad de login

---
+++ src/database/login.js
@@ -0,0 +1,65 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC0zer2obvU5MQrJ2Pe-8_MMCzuwbB-ItU",
+    authDomain: "tutoria-electron.firebaseapp.com",
+    projectId: "tutoria-electron",
+    storageBucket: "tutoria-electron.appspot.com",
+    messagingSenderId: "933581286361",
+    appId: "1:933581286361:web:bbfe963bc60d1a770fc7c8"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+    
+const db = firebase.firestore();
+
+const formLoginAdmin        = document.querySelector('#formLoginAdmin')
+const formNameAdmin         = document.querySelector('#nameAdmin')
+const formPasswordAdmin     = document.querySelector('#passwordAdmin')
+
+const formLoginDocente      = document.querySelector('#formLoginDocente')
+const formNameDocente       = document.querySelector('#nameDocente')
+const formPasswordDocente   = document.querySelector('#passwordDocente')
+
+
+formLoginAdmin.addEventListener('submit', async e => {
+    e.preventDefault();
+    const admin = {
+        name: formNameAdmin.value,
+        password: formPasswordAdmin.value,
+    }
+    try {
+        if(admin.name !== ''){
+            const document = {
+                name: '',
+                password: '',
+            }
+            const collectionAdmin = await db.collection("admin").get().then((querySnapshot) => {
+                console.log(querySnapshot);
+                querySnapshot.forEach((doc) => {
+                    if(doc.data().name === admin.name){
+                        document.name = doc.data().name
+                        document.password = doc.data().password
+                    }
+                });
+            });
+            if(document.name === ''){
+                alert("ERROR: No existe administrador");
+            }else{
+                if(document.password === admin.password){
+                    console.log("password correcto");
+                    window.location="../tutorandos/tutorandos.html"; 
+                }else{
+                    alert("ERROR: Error en contraseña");
+                }
+            }
+        }else{
+            alert("ERROR: Campo nombre está vacío");
+        }
+        
+    } catch (error) {
+        console.log('Error encontrado: ',error);
+    }
+
+    formLoginAdmin.reset();
+});
